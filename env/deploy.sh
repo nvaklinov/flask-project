1619b060e381faa8cbc8bc853b7cef98b0f466d3
EC2 Default User <ec2-user@ip-172-31-33-126.us-east-2.compute.internal>
2020-04-05 18:15:14 +0000

    update readme

--- env/deploy.sh
+++ env/deploy.sh
@@ -4,5 +4,5 @@ $(aws ecr get-login --region us-east-2 --no-include-email)
 
 docker push $REPO:latest
 
-kubectl create -f pod.yaml; status=$?
+kubectl create -f env/pod.yaml; status=$?
 echo $status
