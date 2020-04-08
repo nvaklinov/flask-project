d7dd9593ad09fdd6b4c6f9129b7b33a30f88073b
Nataliya Yochkova <Nataliya Yochkova@example.com>
2020-04-08 17:04:42 +0000

    adding pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -5,8 +5,6 @@ REPO="206354132017.dkr.ecr.us-east-2.amazonaws.com/test-repo"
 
 sudo pip install -r requirements.txt && nohup sudo python web.py &
 
-nohub sudo python web.py &
-
 sudo docker build -t "$REPO:${BUILD_NUMBER}"
 
 sudo docker push "$REPO:${BUILD_NUMBER}"
