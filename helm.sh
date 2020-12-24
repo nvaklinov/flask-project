209becd344c671df1e71d5999569117b4cbce343
Snuffules <snuff.mcloud@gmail.com>
2020-12-24 19:09:50 +0200

    v.1.0.1+helm

--- helm.sh
+++ helm.sh
@@ -1,3 +1,3 @@
 #!bin/bash/
 
-helm upgrade flaskapp helm/ --install --atomic --wait --set deployment.tag=$GIT_COMMIT
+helm upgrade flaskapp flaskapp/ --install --atomic --wait --set deployment.tag=$GIT_COMMIT
