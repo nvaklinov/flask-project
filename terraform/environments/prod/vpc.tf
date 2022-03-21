e7da18834b2ff5451a61e4565a37549422a70387
Your Name <you@example.com>
2022-03-21 14:21:18 +0200

    vpc

--- terraform/environments/prod/vpc.tf
+++ terraform/environments/prod/vpc.tf
@@ -9,7 +9,7 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
     enable_nat_gateway     = true
-    single_nat_gateway     = true
+#    single_nat_gateway     = true
     one_nat_gateway_per_az = false
     enable_dns_hostnames   = true
     reuse_nat_ips          = true
