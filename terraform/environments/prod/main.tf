b13399719eb25a5e6b943df5a1593fcbad096ef0
Your Name <you@example.com>
2022-03-21 11:20:07 +0200

    delete some arguments from vars-prod.tf and main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,7 +1,7 @@
-form{
-  required_providers{
-    aws{
-      source = "hashicorp/aws"
+terraform {
+  required_providers {
+    aws {
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
@@ -10,8 +10,4 @@ form{
 #Configure the AWS Provider
 provider "aws"{
   region = var.region_prod
-}
-
-module "vpc" {
-  source = "modules/vpc"
 }
--- terraform/environments/prod/vars-prod.tf
+++ terraform/environments/prod/vars-prod.tf
@@ -7,10 +7,7 @@ variable "cidr_block_prod" {
 variable "vpc_name_prod" {
   default = "vpc-prod"
 }
-variable "cidr_subnet_prod" {
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
+
 variable "subnet_name_prod" {
   default = "subnet-prod"
 }
