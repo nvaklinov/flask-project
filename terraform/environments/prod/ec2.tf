0d2ed726bae0b1310754b800e3e7567a9c3af814
Your Name <you@example.com>
2022-03-21 11:56:09 +0200

    fix  ec2.tf

--- terraform/environments/prod/ec2.tf
+++ terraform/environments/prod/ec2.tf
@@ -2,8 +2,8 @@ data "aws_ami" "amazon-2" {
   most_recent = true
 
   filter {
-    name   = "owner-alias"
-    values = ["amzn-2-ami-hvm-*-x86_64-ebs"]
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
   owners = ["amazon"]
 }
