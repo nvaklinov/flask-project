37d6582f25dc8e778a151ae8907c77742176ba5b
Your Name <you@example.com>
2022-03-21 12:22:33 +0200

    fix  ec2.tf

--- terraform/environments/prod/ec2.tf
+++ terraform/environments/prod/ec2.tf
@@ -13,20 +13,32 @@ resource "aws_security_group" "allow_jenkins"{
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    description = "TLS from VPC"
-    from_port = 8080
-    protocol  = "tcp"
-    to_port   = 8080
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  tags = {
+    Name = "jenkins_server"
+  }
 }
 resource "aws_instance" "jenkins" {
   ami = data.aws_ami.amazon-2.id
