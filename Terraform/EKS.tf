7fbbbcd99213a581ae7dffc88af08916e2b83c38
EC2 Default User <ec2-user@ip-172-31-37-71.eu-central-1.compute.internal>
2022-04-03 06:26:30 +0000

    EKS file adjustment

--- Terraform/EKS.tf
+++ Terraform/EKS.tf
@@ -8,7 +8,7 @@
 
 module "eks-cluster" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-cluster"
+  cluster_name    = "FlaskApp"
 # cluster_version = "1.21.5"
   subnet_ids          = ["subnet-05b45d1f030bb0f65","subnet-0cd73545e17667d5d","subnet-061ae723ff3b547a0"]
   vpc_id           = "vpc-0e837a81fd41bd5df"
