36c81cb6b6897f7074bbb7e9662b0e1cb83a3a3d
Andy <112399592+Andisimo1@users.noreply.github.com>
2023-06-23 15:47:41 +0300

    Update variables.tf

--- Final/variables.tf
+++ Final/variables.tf
@@ -1,18 +1,18 @@
 variable "vpc" {
-  value = "vpc-0f33bb620ccb65c4b"
+  default = "vpc-0f33bb620ccb65c4b"
 }
 variable "subnets" {
-  value = ["subnet-05eedd743e6fe439b", "subnet-0d1ebbe4cfbbbd12c", "subnet-0c6b680d0742f141d"]
+  default = ["subnet-05eedd743e6fe439b", "subnet-0d1ebbe4cfbbbd12c", "subnet-0c6b680d0742f141d"]
 }
 variable "role_arn" {
-  value = "arn:aws:iam::366915744137:role/Admin"
+  default = "arn:aws:iam::366915744137:role/Admin"
 }
 variable "user" {
-  value = "Terraform"
+  default = "Terraform"
 }
 variable "domain" {
-  value = "andydim.click"
+  default = "andydim.click"
 }
 variable "hosted_zone" {
-  value = "Z04545712VDTBF0N6KJBF"
+  default = "Z04545712VDTBF0N6KJBF"
 }
